3b7cb4fabae20d05e5a59cd6399fcf08706f2e4f
Benjamin Cooper <bencooper222@gmail.com>
2017-10-01 14:43:51 -0500

    Made it work!!

--- fun.js
+++ fun.js
@@ -1,4 +1,4 @@
-console.log("Hey!");
+console.log("Hey VH Hackers!");
 
 // you define variables with let, a variable name and then a value
 let foo = 2;
@@ -8,40 +8,48 @@ console.log(foo);
 let bar;
 console.log("Bar is: " + bar); // should be undefined
 
-bar =3;
+bar = 3;
 console.log("Bar is now: " + bar); // should be 3
 
 
 // let's try multiplying
-console.log(foo + '*' + bar + '=' +(foo*bar));
+console.log(foo + '*' + bar + '=' + (foo * bar));
 
 // now you do division!
-console.log(foo + '*' + bar + '=' +('ADD A DIVISION EXPRESSION HERE'));
+console.log(foo + '/' + bar + '=' + (foo / bar));
 
 
 
 
-function squareNumber(number){ // square a number
-    return Math.pow(number,2); // return is what the function ends up being
+function squareNumber(number) { // square a number
+    return Math.pow(number, 2); // return is what the function ends up being
 }
 
+squareNumber(foo);
 console.log(squareNumber(bar)); // should be 9
 
 
-function printSquareNumber(number){ // calls square number and prints the results
+function printSquareNumber(number) { // calls square number and prints the results
     console.log(squareNumber(number)); // no return necessary
 }
 
-
+printSquareNumber(1772);
 
 // call printSquareNumber
 
-function cubeNumber(number){
+function cubeNumber(number) {
     //write this with a return statement (like squareNumber)
+
+    return Math.pow(number,3);
 }
 
-function printCubeNumber(number){
+function printCubeNumber(number) {
     // write this with a console.log() statement!
+
+    console.log(cubeNumber(number));
 }
 
 
+printCubeNumber(76); // or whatever number you want!
+
+// call printCubeNumber
